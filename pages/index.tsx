def4aeae7006be0c5832d77a703e3034289eff5a
diescake <daisuke.kondo@smarthr.co.jp>
2021-06-20 01:58:01 +0900

    🔧 customize top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -18,40 +19,24 @@ export default function Home() {
         <p className={styles.description}>This site is a bundle of my many playgrounds.</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/react-magnifier" passHref>
+            <div className={styles.card}>
+              <h2>react-magnifier &rarr;</h2>
+              <p>🔍 React image zoom component.</p>
+            </div>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          <a href="https://nextjs.org/" className={styles.card}>
+            <h2>Next.js &rarr;</h2>
+            <p>The React Framework for Production.</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
+        <a href="https://github.com/diescake/hello-worlds" target="_blank" rel="noopener noreferrer">
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/images/github_logo.png" alt="GitHub Logo" width={16} height={16} />
           </span>
         </a>
       </footer>
