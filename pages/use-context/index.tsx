4ee7b00dbfa0e426ab1dc04ce88530ed5a6c6447
Daisuke Kondo <daisuke.kondo@smarthr.co.jp>
2021-09-12 17:51:58 +0900

    🔧 add a log to see which components are being re-rendered

--- pages/use-context/index.tsx
+++ pages/use-context/index.tsx
@@ -8,6 +8,8 @@ import styles from '../../styles/Home.module.css'
 const MyContext = createContext({ count: 0 })
 
 const Child: VFC<{ setCount: (count: number) => void }> = ({ setCount }) => {
+  console.log('Child rendered.')
+
   const data = useContext(MyContext)
 
   return (
@@ -25,18 +27,30 @@ const Child: VFC<{ setCount: (count: number) => void }> = ({ setCount }) => {
   )
 }
 
+const ChildOutOfContext = () => {
+  console.log('ChildOutOfContext rendered.')
+  return <></>
+}
+
 const Parent = () => {
+  console.log('Parent rendered.')
+
   const [count, setCount] = useState(0)
 
   return (
-    <MyContext.Provider value={{ count }}>
-      <Child setCount={setCount} />
-    </MyContext.Provider>
+    <>
+      <MyContext.Provider value={{ count }}>
+        <Child setCount={setCount} />
+      </MyContext.Provider>
+      <ChildOutOfContext />
+    </>
   )
 }
 
 
 export default function Home() {
+  console.log('Home rendered.')
+
   return (
     <div className={styles.container}>
       <Head>
