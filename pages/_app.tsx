83da206064f39661b6bb63c1f592b9c6c51bcffb
diescake <daisuke.kondo@smarthr.co.jp>
2022-03-05 20:56:23 +0900

    refactor: remove globals.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
-
-import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,9 +10,29 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <meta name="twitter:creator" content="diescake" />
         <meta name="twitter:card" content="summary" />
       </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 
 export default MyApp
